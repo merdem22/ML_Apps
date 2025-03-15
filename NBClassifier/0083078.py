0e6b4611435c558f5865d1b62ad1ab1edaa8ad3b
merdem22 <merdem22@ku.edu.tr>
2025-03-15 15:02:19 +0300

    L1 and L2 added, first two steps in NBC.

--- NBClassifier/0083078.py
+++ NBClassifier/0083078.py
@@ -19,6 +19,18 @@ print(y_test.shape)
 # should return a numpy array with shape (K,)
 def estimate_prior_probabilities(y):
     # your implementation starts below
+    #goal: class_priors[i] = Pr(y=i+1)
+    #the number of classes is 20, so initailize with that size 
+
+    K = 20
+    N = len(y)
+
+    class_priors = np.zeros(K)
+
+    for label in y:
+        class_priors[int(label) - 1] +=1
+
+    class_priors/=N #divide each label sum with N to get class mean.
     
     # your implementation ends above
     return(class_priors)
@@ -33,7 +45,24 @@ print(class_priors)
 # should return a numpy array with shape (K, D)
 def estimate_success_probabilities(X, y):
     # your implementation starts below
+    a = 0.2
+    K = 20
+    D = 2000
+
+    class_counts = np.zeros(K) #for Nc.
+
+    #goal: P[i][j] = the probability of word j occuring in a document of class i. Pr(xj = 1 | Y = ci) ?
+    # 1 <= j <= 2000,  1<= i <=20
+    P = np.zeros((K, D))
+
+    for x, y in zip(X, y): #iterate over the data points (zip allows us to reference the label for that given x.)
+
+
     
+
+    
+
+
     # your implementation ends above
     return(P)
 
@@ -41,7 +70,7 @@ P = estimate_success_probabilities(X_train, y_train)
 print(P)
 
 
-
+"""
 # STEP 5
 # assuming that there are N data points and K classes
 # should return a numpy array with shape (N, K)
@@ -73,3 +102,5 @@ print("Training accuracy is {:.2f}%.".format(100 * np.sum(np.diag(confusion_trai
 
 confusion_test = calculate_confusion_matrix(y_test, scores_test)
 print("Test accuracy is {:.2f}%.".format(100 * np.sum(np.diag(confusion_test)) / np.sum(confusion_test)))
+"
+"""
